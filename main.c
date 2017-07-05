4ec3990b277dbe602af7e4b8415b6b02e0b06b7d
y0ubat <y0u_bat@naver.com>
2017-07-05 02:53:04 -0700

    ..

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sum.h>
+
+int main()
+{
+	int a = sum(100);
+	printf("[+] sum : %d\n",a);
+	return 0;
+}
--- sum.c
+++ sum.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <sum.h>
 
 int sumre = 0;
